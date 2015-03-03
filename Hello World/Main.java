ff92e0e230f7a43904343eda448f069619e0a6c4
rozsnyaitamas <rozsthomas@gmail.com>
2015-03-03 18:02:53 +0200

    Update Main.java

--- Hello World/Main.java
+++ Hello World/Main.java
@@ -1,6 +1,6 @@
 public class Main{
 	public static void main(String argument[]){
-		Thing World=new Thing("Hello World!");//csinalj egy class-t ez alapjan amit it csinaltam :P
+		Thing World=new Thing("Hello World!");//csinalj egy class-t ez alapjan amit itt csinaltam :P
 		
 		System.out.println(World.Say);
 	}
